9ef26fb0f76db60827b933260514d605b1c098ee
Jan Andreasen <jan@tved.it>
2022-01-17 14:00:38 +0100

    Razor igen igen - it builds

---
+++ Console Client/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Grpc.Net.Client;
+using Web_Server;
+
+namespace Console_Client
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            using var channel = GrpcChannel.ForAddress("https://127.0.0.1:5000");
+            var greeter = new Greeter.GreeterClient(channel);
+        }
+    }
+}
---
+++ Web Server/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Web_Server.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.WebHost.UseKestrel(
+    options =>
+    {
+        options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+    });
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
